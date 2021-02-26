dacc248fecdb05a31424a9bb4cc0eddfdfe8cc07
MohammedIjaz7 <78848803+MohammedIjaz7@users.noreply.github.com>
2021-02-26 17:24:06 +0530

    testing

--- mini_project.py
+++ mini_project.py
@@ -1,9 +1,9 @@
 '''
 Description of the code:- This code is used to search the words asked by the
-user and based on the number of keywords entered, a separate 'keyword.txt' file 
-is created where the keyword and its adjacent words are written in new line for 
-each word in respective files. In case of entered keyword not available in the file
-it displays appropriate error message for that.
+user and based on the number of keywords entered, a separate 'keyword.txt' file
+is created where the keyword and its adjacent words are written in new line for
+each word in respective files. In case of entered keyword not available in the
+file it displays appropriate error message for that.
 
 Author: Mohammed Ijaz
 Contact: mohammed.ijaz@ltts.com
@@ -19,16 +19,17 @@ import os
 
 class Classfirst:
 
-    def __init__(self): # user input for no of search operation
+    def __init__(self):  # user input for no of search operation
         self.flag = True
-        self.nos = input("Enter the number of keyword you want to search:\n")   # number of words searched
-        while self.flag:    # Read only integer input from user else keep reading
+        # number of words searched
+        self.nos = input("Enter the number of keyword you want to search:\n")
+        # Read only integer input from user else keep reading
+        while self.flag:
             if self.nos.isdigit():
                 self.flag = False
             else:
                 print("Please enter the correct input value")
-                self.nos = input("Enter the number of keyword you want to
-                                 search: \n")
+                self.nos = input("Enter the number of keyword to search: \n")
 
 
 class Searchclass(Classfirst):  # class to perform search operation
@@ -51,14 +52,15 @@ class Searchclass(Classfirst):  # class to perform search operation
 
             read_file1 = read_file.readlines()
 
-            for line in read_file1: # append each line of text file to string
+            for line in read_file1:  # append each line of text file to string
                 smt += line
 
-            smt = re.sub(r'\W+', ' ', smt)  # removing special characters and new lines from string
+            # removing special characters and new lines from string
+            smt = re.sub(r'\W+', ' ', smt)
 
             newstr = smt.split(' ')
-
-            for x in range(len(newstr)):    # itterating through out the string and searching for keyword
+            # itterating through out the string and searching for keyword
+            for x in range(len(newstr)):
 
                 if newstr[x].lower() == Search_key.lower():
                     write_file.write(newstr[x - 1] + ' ' + newstr[x] +
@@ -72,7 +74,8 @@ class Searchclass(Classfirst):  # class to perform search operation
 
             write_file.close()
 
-            if count == 0:  # if no word was found delete empty file created previously
+            # if no word was found delete empty file created previously
+            if count == 0:
 
                 print("Keyword not found in the file\n")
 
